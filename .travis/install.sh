a2eed58e7ec9579b2e50c25196c844ab1bd27087
Jamie Liu <jliu@nixprime.com>
2017-02-11 17:15:31 -0800

    Fix Travis OSX builds

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,9 +8,9 @@ if [[ "${TRAVIS_OS_NAME}" == 'osx' ]]; then
     brew update
     # Skip updating Boost, since doing so takes a long time and we'd like to
     # know about compatibility breakage anyway.
-    brew install cmake
+    brew install cmake || brew outdated cmake || brew upgrade cmake
     case "${TEST_PY}" in
-        py2) brew outdated python || brew upgrade python;;
-        py3) brew install python3;;
+        py2) brew install python || brew outdated python || brew upgrade python;;
+        py3) brew install python3 || brew outdated python3 || brew upgrade python3;;
     esac
 fi
