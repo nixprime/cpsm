45676e7f836a13c194fb8ab1a53e30b250333f31
Jamie Liu <jliu@nixprime.com>
2018-01-31 09:07:43 -0800

    Use python-config script to detect Python flags
    (aka "rewrite Python library finding, again")
    
    Arch, and possibly others, places the Python .so outside of LIBPL;
    there's a .a in LIBPL instead which FindPythonLibsCustom locates,
    causing the resulting cpsm_py.so to be linked against the wrong
    library (and also missing libutil, which causes `import cpsm_py`
    to fail due to a missing `forkpty`). Use the python-config script
    provided by the CPython distribution instead.
    
    Fixes #35, #40

---
+++ cmake/FindPythonConfig.cmake
@@ -0,0 +1,28 @@
+# FindPythonConfig
+# ----------------
+#
+# This module locates Python libraries.
+#
+# This code sets the following variables:
+#
+# PYTHONCONFIG_FOUND - have the Python libs been found
+# PYTHON_COMPILE_FLAGS - compiler flags required to include Python headers
+# PYTHON_LINK_FLAGS - linker flags required to link Python libraries
+#
+# If calling both `find_package(PythonInterp)` and
+# `find_package(PythonConfig)`, call `find_package(PythonInterp)` first.
+
+include(FindPackageHandleStandardArgs)
+
+find_package(PythonInterp)
+if(PYTHONINTERP_FOUND)
+  string(CONCAT _Python_config ${PYTHON_EXECUTABLE} "-config")
+  execute_process(COMMAND ${_Python_config} "--includes" OUTPUT_VARIABLE PYTHON_COMPILE_FLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
+  execute_process(COMMAND ${_Python_config} "--ldflags" OUTPUT_VARIABLE PYTHON_LINK_FLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
+  string(CONCAT _Python_config_message ${PYTHON_COMPILE_FLAGS} "; " ${PYTHON_LINK_FLAGS})
+  unset(_Python_config)
+else(PYTHONINTERP_FOUND)
+  message(SEND_ERROR "Python interpreter not found")
+endif(PYTHONINTERP_FOUND)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(PythonConfig DEFAULT_MSG _Python_config_message PYTHON_COMPILE_FLAGS PYTHON_LINK_FLAGS)
+unset(_Python_config_message)
